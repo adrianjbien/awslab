a5e4a2335c5a309e28b41573446a6e573a767897
Adrian <247630@edu.p.lodz.pl>
2026-04-18 15:24:32 +0200

    lab1_2

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,11 +1,46 @@
-//stub for lab 1_2
+// stub for lab 1_2
 var AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('./config.json');
 
-var task =  function(request, callback){
-	
-	
-}
+var ec2 = new AWS.EC2();
 
-exports.lab = task
+var task = function (request, callback) {
+
+    var params = {
+        // możesz dodać filtry jeśli chcesz, np. tylko running:
+        // Filters: [{ Name: "instance-state-name", Values: ["running"] }]
+    };
+
+    ec2.describeInstances(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(request, {
+                status: 500,
+                body: JSON.stringify(err)
+            });
+        } else {
+            // upraszczamy odpowiedź (czytelniejsze dane)
+            var instances = [];
+
+            data.Reservations.forEach(reservation => {
+                reservation.Instances.forEach(instance => {
+                    instances.push({
+                        InstanceId: instance.InstanceId,
+                        State: instance.State.Name,
+                        Type: instance.InstanceType,
+                        PublicIP: instance.PublicIpAddress,
+                        LaunchTime: instance.LaunchTime
+                    });
+                });
+            });
+
+            callback(request, {
+                status: 200,
+                body: JSON.stringify(instances, null, 2)
+            });
+        }
+    });
+};
+
+exports.lab = task;
