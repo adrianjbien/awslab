0c8a2b33267b16a68fe8cd678537991d1e2c33fc
Adrian <247630@edu.p.lodz.pl>
2026-04-19 13:21:22 +0200

    wersja debug

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -15,7 +15,6 @@ var task = function (request, callback) {
 
         if (err) {
             console.log("AWS ERROR:", err);
-
             callback(request, {
                 status: 500,
                 body: JSON.stringify(err)
@@ -23,14 +22,27 @@ var task = function (request, callback) {
             return;
         }
 
-        console.log("AWS DATA:", JSON.stringify(data));
+        var result = [];
+
+        data.Reservations.forEach(res => {
+            res.Instances.forEach(inst => {
+                result.push({
+                    id: inst.InstanceId,
+                    state: inst.State.Name,
+                    type: inst.InstanceType,
+                    publicIp: inst.PublicIpAddress,
+                    publicDns: inst.PublicDnsName,
+                    launchTime: inst.LaunchTime
+                });
+            });
+        });
+
+        console.log("PARSED:", result);
 
         callback(request, {
             status: 200,
-            body: JSON.stringify(data, null, 2)
+            body: JSON.stringify(result, null, 2)
         });
-
-        console.log("AWS request finished");
     });
 };
 
